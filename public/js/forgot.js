7edce001c78ff7b4ae504bc6ce2a8e992eba9ca5
pappagnu <andreas@klonad.se>
2017-08-02 13:52:13 +0200

    loading indicator added for forgot password form

--- public/js/forgot.js
+++ public/js/forgot.js
@@ -1,12 +1,14 @@
 const forgotPassForm = document.getElementById('form-forgot')
 const forgotPassMsg = document.getElementById('forgot')
 const email = document.getElementById('email')
+const loading = document.getElementById('loading-container')
 
 forgotPassForm.addEventListener('submit', async function (e) {
   e.preventDefault()
   forgotPassMsg.attributes[1].nodeValue = ''
   email.className = 'validate'
   try {
+    loading.classList.add('active')
     const response = await fetch('/forgot', {
       headers: new Headers({
         'Content-Type': 'application/json; charset=utf-8'
@@ -22,9 +24,11 @@ forgotPassForm.addEventListener('submit', async function (e) {
       setTimeout(() => {
         window.location.href = '/'
       }, 3000)
+      loading.classList.remove('active')
     } else {
       email.className = 'validate invalid'
       forgotPassMsg.attributes[1].nodeValue = data.message
+      loading.classList.remove('active')
     }
   } catch (err) {
     console.log(err)
