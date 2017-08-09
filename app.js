e177cc534f0bbdef7da7152cdef6c6073d688f56
pappagnu <andreas@klonad.se>
2017-08-09 12:56:31 +0200

    small fix for session

--- app.js
+++ app.js
@@ -21,7 +21,9 @@ app.use(session({
   secret: process.env.SESSION_SECRET,
   saveUninitialized: false,
   resave: false,
-  cookie: { secure: !true },
+  cookie: {
+    secure: !true
+  },
   store: new MongoStore({
     mongooseConnection: mongoose.connection,
     ttl: 14 * 24 * 60 * 60
