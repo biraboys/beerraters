a725fb0e1778a0cf80bbc9b4021c0a5304490b2e
pappagnu <andreas@klonad.se>
2017-08-10 16:44:35 +0200

    status indicator to show if user is online/offline in user profile

--- public/js/login.js
+++ public/js/login.js
@@ -25,28 +25,23 @@ loginForm.addEventListener('submit', async function (e) {
     const data = await response.json()
 
     if (data) {
-      console.log(data)
       if (data.active && data.message[0] === 'P') {
 				// Password does not match
         password.className = 'validate invalid'
         passwordMsg.attributes[2].nodeValue = data.message
         loginBtn.classList.remove('disabled')
-        console.log(data)
       } else if (data.active === false) {
 				// User account not activated
         username.className = 'validate invalid'
         usernameMsg.attributes[2].nodeValue = data.message
         loginBtn.classList.remove('disabled')
-        console.log(data)
       } else if (data.message[0] === 'C') {
 				// No user found
         username.className = 'validate invalid'
         usernameMsg.attributes[2].nodeValue = `${data.message} '${username.value}'`
         loginBtn.classList.remove('disabled')
-        console.log(data)
       } else {
-        location.href = document.referrer
-        console.log(data.message)
+        location.href = '/'
       }
     }
   } catch (err) {
